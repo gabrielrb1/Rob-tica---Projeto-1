2661813863faf06fcc5b337d44dcedcb7cce049a
Frederico Wernesbach <frederico.wernesbach@etaure.com.br>
2018-10-01 19:21:20 -0300

    Modificações no teste.m

--- teste.m
+++ teste.m
@@ -5,16 +5,25 @@ clc
 cam = webcam('HD Pro Webcam C920');
 cam.Resolution = '2304x1536';
 imrgb = snapshot(cam);
-%imrgb = imread('foto_real.jpg');
+%imrgb = imread('orientacao.jpg');
 img_hsv = rgb2hsv(imrgb);
-imshow(img_hsv);
-[x,y] = ginput(2);
-imcortada = img_hsv(y(1):y(2),x(1):x(2));
-figure
-imshow(imcortada);
-img_h = imcortada(:,:,1);
-%img_s = imcortada(:,:,2);
-%img_v = imcortada(:,:,3);
+txt = cell(1,1);
+txt{1,1} = ['Escolha na ordem:' newline '1- Frente', newline '2- Traseira', newline '3- Alvo'];
+pos = [10 180];
+image = insertText(img_hsv,pos,txt,'FontSize',50);
+imshow(image);
+maxI = cell(1,3);
+minI = cell(1,3);
+for ii=1:3
+    [x,y] = ginput(2);
+    imcortada = img_hsv(y(1):y(2),x(1):x(2));
+    %figure
+    %imshow(imcortada);
+    img_h = imcortada(:,:,1);
+    %img_s = imcortada(:,:,2);
+    %img_v = imcortada(:,:,3);
 
-max(max(img_h))
-min(min(img_h))
+    maxI{ii} = max(max(img_h));
+    minI{ii} = min(min(img_h));
+end
+close(gcf);
